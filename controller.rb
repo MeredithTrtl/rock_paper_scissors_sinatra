758591bb866c68cee4a41ea2f76bcf59abad43f7
Meredith Gallop <meredith@trtl.co.uk>
2020-01-27 23:07:06 +0000

    added css, updated win/lose messages to reflect player who won or lost

--- controller.rb
+++ controller.rb
@@ -8,7 +8,15 @@ get '/'do
   erb(:home)
 end
 
+get '/play' do
+  erb(:play)
+end
+
 get '/:thing1/:thing2' do
   @winner = "#{Game.compare(params[:thing1], params[:thing2])}"
   erb(:result)
+end
+
+post '/game' do
+  redirect to("/#{params[:P1].to_s.downcase}/#{params[:P2].to_s.downcase}")
 end
--- models/game.rb
+++ models/game.rb
@@ -2,21 +2,20 @@ class Game
 
   def self.compare(thing1, thing2)
 
-    if (thing1 == 'rock' && thing2 == 'paper') || (thing1 == 'paper' && thing2 == 'rock')
-      return 'paper'
-    end
+    p1_wins = (thing1 == 'paper' && thing2 == 'rock') || (thing1 == 'scissors' && thing2 == 'paper') || (thing1 == 'rock' && thing2 == 'scissors')
+    p2_wins = (thing1 == 'rock' && thing2 == 'paper') || (thing1 == 'paper' && thing2 == 'scissors') || (thing1 == 'scissors' && thing2 == 'rock')
+    draw = (thing1 == 'scissors' && thing2 == 'scissors') || (thing1 == 'paper' && thing2 == 'paper') || (thing1 == 'rock' && thing2 == 'rock')
 
-    if (thing1 == 'scissors' && thing2 == 'paper') || (thing1 == 'paper' && thing2 == 'scissors')
-      return 'scissors'
-    end
 
-    if (thing1 == 'scissors' && thing2 == 'rock') || (thing1 == 'rock' && thing2 == 'scissors')
-      return 'rock'
+    if p1_wins
+      return "Player 1 wins, with an incredible play of #{thing1} against Player 2's puny little #{thing2}."
+    elsif p2_wins
+      return "Player 2 has defeated Player 1's #{thing1} with their mighty #{thing2}, Oh Player 1, how embarassing"
+    elsif draw
+      return 'No-one won, you drew :/. Go home and think about what you did.'
+    else
+      return "#{thing1.capitalize} vs. #{thing2.capitalize} ??? Are you kidding? Try again..."
     end
-
-    if (thing1 == 'scissors' && thing2 == 'scissors') || (thing1 == 'paper' && thing2 == 'paper') || (thing1 == 'rock' && thing2 == 'rock')
-      return 'no-one, you drew :/'
-    end
-
   end
+
 end
